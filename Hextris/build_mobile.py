5d72d00b151e2b5638bbd311f8e1427ef563bbe9
meadowstream <meadowstream@users.noreply.github.com>
2014-07-18 21:24:07 -0400

    added more

--- Hextris/build_mobile.py
+++ Hextris/build_mobile.py
@@ -5,7 +5,7 @@ urllib.urlretrieve ("https://github.com/Hextris/hextris/archive/gh-pages.zip", "
 print "Extracting..."
 
 with zipfile.ZipFile('./gh-pages.zip', "r") as z:
-    z.extractall("./www/")
+	z.extractall("./www/")
 
 os.remove('./www/hextris-gh-pages/.gitignore')
 os.remove('./www/hextris-gh-pages/README.md')
@@ -14,13 +14,11 @@ fs = os.listdir('./www/hextris-gh-pages/')
 for i in fs:
 	shutil.move('./www/hextris-gh-pages/' + i, './www/')
 
-with open('./www/js/initialization.js', 'w+') as f:
-	s = f.read().split('\n')
-	s[0] = 'document.addEventListener(\'deviceready\',initialize, false);'
-	ns = ''
-	for i in s:
-		ns += i + '\n'
-	f.write(ns)
+with open('./www/js/initialization.js', 'r') as fin:
+	data = fin.read().splitlines(True)
+with open('./www/js/initialization.js', 'w') as fout:
+	data[0] = 'document.addEventListener(\'deviceready\',initialize, false);'
+	fout.writelines(data)
 
 print "Cleaning up..."
 os.rmdir('./www/hextris-gh-pages/')
